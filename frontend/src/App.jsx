7d865204adb0c0b91a16e9369fc2965ad12777e1
mcbauman <m.c.baumann@icloud.com>
2022-05-18 14:42:56 +0200

    small chnage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,6 @@ import env from "react-dotenv"
 import react from "react"
 
 export default function App(){
-    console.log(env);
-    console.log(env.MARVEL_PUBLIC_KEY);
-    console.log(window.env.MARVEL_PUBLIC_KEY);
-
     // fetch(process.env.MARVELAPI,{apikey:process.env.MARVEL_PUBLIC_KEY})
     return(
         <>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,10 @@ import reactDom from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 
-import env from "react-dotenv"
-import react from "react"
+
 
 import "./style.scss"
 // import dotenv from "dotenv"
 
-console.log(env.MARVEL_PUBLIC_KEY);
-console.log(window.env.MARVEL_PUBLIC_KEY);
 
 reactDom.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById("root"))
