705e607a8d860d074d752c38dcc2bf3f3f9e511c
mcbauman <m.c.baumann@icloud.com>
2022-06-01 23:15:04 +0200

    some Styling

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -3,10 +3,8 @@ import {NavLink} from "react-router-dom"
 export default function Header(){
    return(
        <header>
-           HEADER
-           "nav"
            <nav>
-               <NavLink to="Start">Start</NavLink>
+               <NavLink to="Start">friendcircle.de</NavLink>
                <NavLink to="Search">Search</NavLink>
                <NavLink to="Messages">Messages</NavLink>
                <NavLink to="Profile">Profile</NavLink>
