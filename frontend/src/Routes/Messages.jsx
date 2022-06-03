38d99c46bbf025ccbae63aa380df84f6240ac123
mcbauman <m.c.baumann@icloud.com>
2022-06-03 10:50:36 +0200

    message System ready

--- frontend/src/Routes/Messages.jsx
+++ frontend/src/Routes/Messages.jsx
@@ -20,15 +20,17 @@ export default function Messages(props){
         requestMessages()
     }, [])
 
-    function writeMessage(id){
+    function writeMessage(id,author){
         setVis(vis?0:id)
         if(vis&&subject.length>1){
             const headers = { Authorization: `Bearer ${props.token}` }
-            const data={subject,content,author:props.user,recipient:id}
+            const data={subject,content,author:props.user,recipient:author}
+///ID Of Message or User?
             axios.post("http://localhost:9000/message/create",data, {headers})
                 .then(res => {
                     setSubject("")
                     setContent("")
+                    requestMessages()
                 })
                 .catch(error => alert(error.response?.data?.error || "Unknown error"))
         }
@@ -40,14 +42,14 @@ export default function Messages(props){
             <section id="messages">
                 {allMsg.map(item=>(
                     <div key={item._id} className="ProfileCard">
-                        <div>{item.author}</div>
+                        <div>{item.author.userName}</div>
                         <div>{item.subject}</div>
                         <div>{item.content}</div>
                         <form className={vis===item._id?"show":"hide"}>
                             <input type="text" placeholder="subject" value={subject} onChange={(e)=>setSubject(e.target.value)}/>
                             <input type="text" placeholder="your text" value={content} onChange={(e)=>setContent(e.target.value)}/>
                         </form>
-                        <button onClick={()=>writeMessage(item._id)}>send Message</button>
+                        <button onClick={()=>writeMessage(item._id,item.author._id)}>send Message</button>
                     </div>
                 ))}
             </section>
