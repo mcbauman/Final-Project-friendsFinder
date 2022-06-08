8990aa8ef21d211f9455f2a5355057ea518f6ff9
mcbauman <m.c.baumann@icloud.com>
2022-06-08 15:20:25 +0200

    bugfixing

--- frontend/src/Routes/Profile.jsx
+++ frontend/src/Routes/Profile.jsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import Activities from "../ActivitiesArray";
 import axios from "axios";
 
-export default function Profile(props) {
-  const [file, setFile] = useState(null);
-
+export default function Profile(props){
+    const [file, setFile] = useState(null)
 
     console.log(props);
 
@@ -17,10 +16,12 @@ export default function Profile(props) {
         // const [picture, setPicture ] = useState(null)
         if(!file){
             return alert("Select a file first:)")
-        } 
+        }
         const formData = new FormData()
         formData.append("selectedFile", file)
+
         const headers = { Authorization: `Bearer ${props.token}`}
+
         const config = {
             // data: formData,
             headers
@@ -28,26 +29,15 @@ export default function Profile(props) {
         axios.post(`${process.env.REACT_APP_BE_SERVER}/picture/createPicture`, formData, config )
             .then(result =>  props.setUserProfPic(result))
             .catch(error => console.log(error))
-        
+
     }
 
-      const config = {
-        method: "POST",
-        body: formData,
-      };
-      
-        fetch(`${process.env.REACT_APP_BE_SERVER}/user/createPicture`, config)
-        .then((res) => res.json())
-        .then((res) => res.send({}))
-        .catch();
-    }
-  }
-
-
-            {/* {alert ? <p>{alert}</p> : null} */}
-
+    return(
+        <article>
+            PROFILE
+            <input type="file" onChange={handleSelectedFile} />
+            <button onClick={saveFile}>Save Picture</button>
             <img src={`${process.env.REACT_APP_BE_SERVER}/picture/${props.userProfPic}` }alt="Ups, no picture;)"/>
         </article>
     )
 }
-
