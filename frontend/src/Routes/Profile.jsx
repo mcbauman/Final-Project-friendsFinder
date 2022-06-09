9bc8c074b721434791dfa1568a832da1377a1a84
mcbauman <m.c.baumann@icloud.com>
2022-06-09 21:54:04 +0200

    frotnend Change User

--- frontend/src/Routes/Profile.jsx
+++ frontend/src/Routes/Profile.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Activities from "../ActivitiesArray";
 import axios from "axios";
+import Select from "react-select";
+import {MdOutlineDeleteForever} from "react-icons/md";
 
 export default function Profile(props){
     const [file, setFile] = useState(null)
@@ -26,6 +28,9 @@ export default function Profile(props){
 
     console.log("USER PROFILE from PROFILE.JSX L 34",props.userProfPic)
 //    console.log("USER PROFILE ID from PROFILE.JSX L 34",props.userProfPic.data._id)
+    
+    // {friends.map(item=><div>{item.picture}</div><div>{item.name}</div><button><MdOutlineDeleteForever/></button>)}
+    
     return(
         <article>
             PROFILE
@@ -38,6 +43,19 @@ export default function Profile(props){
             <input type="file" onChange={handleSelectedFile} />
             <button onClick={saveFile}>Save Picture</button>
             {props.userProfPic&&<img src={`${process.env.REACT_APP_BE_SERVER}/picture/${props.userProfPic}`} alt="Ups, no picture;)"/>}
+            <hr/>
+            <form>
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="family name" />
+                <input type="email" placeholder="@" />
+                <input type="password" placeholder="password" />
+                <input type="password" placeholder="password" />
+                <textarea/>
+                <hr/>
+                <Select/>
+            </form>
+            <hr/>
+           
         </article>
     )
 }
