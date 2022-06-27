6860aeec3bea97292c8298b2550800c31e6a28c0
mcbauman <m.c.baumann@icloud.com>
2022-06-27 19:20:41 +0200

    deletet Old Messages

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -26,7 +26,7 @@ export default function Header(props){
                <NavLink to="*"><span className="bigScreen"><img src={logo} id="logo871" /> friendscircle.de</span>
                <img src={logo} id="logo871" className="smallScreen"/></NavLink>
                <NavLink to="Search"> <span className="bigScreen">{trans[lang].Search}</span><MdSearch className="smallScreen"/></NavLink>
-               <NavLink to="Messages"> <span className="bigScreen">{trans[lang].Messages}</span><MdOutlineEmail className="smallScreen"/> </NavLink>
+               {/* <NavLink to="Messages"> <span className="bigScreen">{trans[lang].Messages}</span><MdOutlineEmail className="smallScreen"/> </NavLink> */}
                <NavLink onClick={()=>setHide(false)}to="Chats"> <span className="bigScreen">{trans[lang].Messages}</span><MdOutlineEmail className="smallScreen"/></NavLink>
                <NavLink to="Profile"> <span className="bigScreen">{trans[lang].Profile}</span><CgProfile className="smallScreen"/></NavLink>
                <MdLogout onClick={logout}/>
