2d92b497cb4acf2d36007c7c7a33b05074929b06
henrik.kuerschner <henrik@thenewsoulsearchers.de>
2022-07-08 11:52:58 +0200

    henne

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,28 +35,28 @@ export default function App(){
       localStorage.setItem("userProfPic",(userProfPic))
     },[userProfPic])
 
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-    function success(pos) {
-      const crd = pos.coords;
+   //  const options = {
+   //    enableHighAccuracy: true,
+   //    timeout: 5000,
+   //    maximumAge: 0
+   //  };
+   //  function success(pos) {
+   //    const crd = pos.coords;
       
-      console.log('Your current position is:');
-      console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude: ${crd.longitude}`);
-      console.log(`More or less ${crd.accuracy} meters.`);
-      setLatitude(crd.latitude);
-      setLangitude(crd.longitude);
+   //    console.log('Your current position is:');
+   //    console.log(`Latitude : ${crd.latitude}`);
+   //    console.log(`Longitude: ${crd.longitude}`);
+   //    console.log(`More or less ${crd.accuracy} meters.`);
+   //    setLatitude(crd.latitude);
+   //    setLongitude(crd.longitude);
 
-      }
-      // console.log(lat,leng)
-      function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
+   //    }
+   //    // console.log(lat,leng)
+   //    function error(err) {
+   //    console.warn(`ERROR(${err.code}): ${err.message}`);
+   //    }
       
-      navigator.geolocation.getCurrentPosition(success, error, options); 
+   //    navigator.geolocation.getCurrentPosition(success, error, options); 
    return(
       <div className={theme}>
          {user&&token?
