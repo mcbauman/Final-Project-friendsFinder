c0c11d364d1eca5b2cbbf3e9373a45e13347d14e
henrik.kuerschner <henrik@thenewsoulsearchers.de>
2022-07-12 11:18:31 +0200

    übersetzungen henrik

--- frontend/src/components/ActivitiesArray.js
+++ frontend/src/components/ActivitiesArray.js
@@ -1,68 +1,85 @@
-const Activities={
-    de:[      
-        {
-            value:"Abnehmen",label:"Abnehmen"
-        },
-        {
-            value:"Campen", label:"Campen"
-        }, 
-        {
-            value:"Fahrradfahren Rennrad",label:"Fahrradfahren Rennrad"
-        },
-        {
-            value:"Fahrradfahren Mountain-Bike",label:"Fahrradfahren Mountain-Bike"
-        },
-        {
-            value:"Fahrradfahren Radreisen",label:"Fahrradfahren Radreisen"
-        },
-        {
-            value:"Kochen", label:"Kochen"
-        },
-        {
-            value:"Laufen",label:"Laufen"
-        },
-        {
-            value:"Schach",label:"Schach"
-        },    
-        {
-            value:"Tanzen",label:"Tanzen"
-        },
-        {
-            value:"Wandern",label:"Wandern"
-        }],
-    en:
-       [ {
-            value:"Abnehmen",label:"Loos Weight"
-        },
-        {
-            value:"Campen", label:"Camping"
-        }, 
-        {
-            value:"Fahrradfahren Rennrad",label:"Fahrradfahren Rennrad"
-        },
-        {
-            value:"Fahrradfahren Mountain-Bike",label:"Fahrradfahren Mountain-Bike"
-        },
-        {
-            value:"Fahrradfahren Radreisen",label:"Fahrradfahren Radreisen"
-        },
-        {
-            value:"Kochen", label:"Kochen"
-        },
-        {
-            value:"Laufen",label:"Laufen"
-        },
-        {
-            value:"Schach",label:"Schach"
-        },    
-        {
-            value:"Tanzen",label:"Tanzen"
-        },
-        {
-            value:"Wandern",label:"Wandern"
-        }]
-}
+const Activities = {
+  de: [
+    {
+      value: "Abnehmen",
+      label: "Abnehmen",
+    },
+    {
+      value: "Campen",
+      label: "Campen",
+    },
+    {
+      value: "Fahrradfahren Rennrad",
+      label: "Fahrradfahren Rennrad",
+    },
+    {
+      value: "Fahrradfahren Mountain-Bike",
+      label: "Fahrradfahren Mountain-Bike",
+    },
+    {
+      value: "Fahrradfahren Radreisen",
+      label: "Fahrradfahren Radreisen",
+    },
+    {
+      value: "Kochen",
+      label: "Kochen",
+    },
+    {
+      value: "Laufen",
+      label: "Laufen",
+    },
+    {
+      value: "Schach",
+      label: "Schach",
+    },
+    {
+      value: "Tanzen",
+      label: "Tanzen",
+    },
+    {
+      value: "Wandern",
+      label: "Wandern",
+    },
+  ],
+  en: [
+    {
+      value: "Abnehmen",
+      label: "Losing Weight",
+    },
+    {
+      value: "Campen",
+      label: "Camping",
+    },
+    {
+      value: "Fahrradfahren Rennrad",
+      label: "Road Bike",
+    },
+    {
+      value: "Fahrradfahren Mountain-Bike",
+      label: "Mountain Bike",
+    },
+    {
+      value: "Fahrradfahren Radreisen",
+      label: "Bike Traveling",
+    },
+    { value: "Kochen", label: "Cooking" },
+    {
+      value: "Laufen",
+      label: "Running",
+    },
+    {
+      value: "Schach",
+      label: "Chess",
+    },
+    {
+      value: "Tanzen",
+      label: "Dancing",
+    },
+    {
+      value: "Wandern",
+      label: "Hiking",
+    },
+  ],
+};
 
-
-
-export default Activities
+export default Activities;
