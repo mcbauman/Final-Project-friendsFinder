4a8859234be1888e6e2e207a31c0c4c955d18812
henrik.kuerschner <henrik@thenewsoulsearchers.de>
2022-07-18 18:02:16 +0200

    henrik start/cover

--- frontend/src/Routes/Welcome.jsx
+++ frontend/src/Routes/Welcome.jsx
@@ -9,9 +9,11 @@ export default function Welcome(props) {
   return (
     <div className={props.disp}>
       <img src="COF.png" alt="the circle of friends" />
-      <h1>the Circle of friends </h1><br/>
+      <h1>the Circle of friends </h1>
       <p>why we came and foremost how we came together</p>
-      <a onClick={click}> come and see your friends here</a>
+      <a className="henrik" onClick={click}>
+        come and see your friends here
+      </a>
     </div>
   );
 }
